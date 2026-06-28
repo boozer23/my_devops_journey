f7f7dd6213a88ae243203655ddb4c9cdf917977f
Vladimir <boozer23k@gmail.com>
2026-06-28 13:21:12 +0300

    Переменные окружения в Docker

--- projects/flask_app/app.py
+++ projects/flask_app/app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+MY_NAME = os.environ.get("MY_NAME", "Незнакомец")
+
 @app.route("/")
 def home():
-    return "Привет! Мой первый веб-сервер работает!"
+    return f"Привет! Меня зовут {MY_NAME}"
 
 @app.route("/about")
 def about():
